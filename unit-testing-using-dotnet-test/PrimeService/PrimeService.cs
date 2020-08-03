5beb2bfdd22523b6f89584e580b6a1d63f642130
avinash-rcs <avinash.rcs@hotmail.com>
2020-08-03 18:49:39 +0530

    third commit

--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -7,7 +7,7 @@ namespace Prime.Services
         public bool IsPrime(int candidate)
         {
             if (candidate ==1) return false;
-            return false;
+            return true;
         }
     }
 }
